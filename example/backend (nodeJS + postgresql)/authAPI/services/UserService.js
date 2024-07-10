9ffdc3d54436f897f2f6628b69c7f27bd2a64b7a
ZaiiiRan <a1b075753@gmail.com>
2024-07-10 16:09:37 +0300

    optimized code and queries

--- example/backend (nodeJS + postgresql)/authAPI/services/UserService.js
+++ example/backend (nodeJS + postgresql)/authAPI/services/UserService.js
@@ -8,18 +8,15 @@ const db = require('../../db')
 
 class UserService {
     async register(username, email, password) {
-        let candidate = await db.query('SELECT * FROM users WHERE username = $1;', [username])
+        const candidate = await db.query('SELECT * FROM users WHERE username = $1 OR email = $2 LIMIT 1;', [username, email])
         if (candidate.rows[0]) {
-            throw ApiError.BadRequest('Пользователь с таким логином уже существует')
-        }
-        candidate = await db.query('SELECT * FROM users WHERE email = $1;', [email])
-        if (candidate.rows[0]) {
-            throw ApiError.BadRequest('Пользователь с таким Email уже существует')
+            throw ApiError.BadRequest(candidate.rows[0].username === username ? 'Пользователь с таким логином уже существует' 
+                : 'Пользователь с таким Email уже существует')
         }
 
         const hashPassword = bcrypt.hashSync(password, 6)
         const activationLink = uuid.v4()
-        const userRole = await db.query('SELECT * FROM roles WHERE value = $1', ['USER'])
+        const userRole = await db.query('SELECT * FROM roles WHERE value = $1;', ['USER'])
 
         const user = await db.query('INSERT INTO users (username, email, password, "activationLink") VALUES ($1, $2, $3, $4) RETURNING *;', [username, email, hashPassword, activationLink])
         await db.query('INSERT INTO roles_for_users ("userID", "roleID") VALUES ($1, $2);', [user.rows[0]._id, userRole.rows[0]._id])
@@ -27,41 +24,21 @@ class UserService {
 
         mailService.sendMail(email, `${process.env.HOST}/auth/activate/${activationLink}`)
 
-        const dto = new UserDTO({...user.rows[0], roles: ['USER']})
-        const tokens = tokenService.generateTokens({ ...dto })
-        await tokenService.saveToken(dto.id, tokens.refreshToken)
-
-        return {
-            ...tokens,
-            user: dto
-        }
+        return this.createResponse(user.rows[0])
     }
 
     async login(username, password) {
-        const user = await db.query('SELECT * FROM users WHERE username = $1;', [username])
-        if (!user.rows[0]) {
+        const user = await this.getUserByUsername(username)
+        if (!user) {
             throw ApiError.BadRequest('Пользователь не найден')
         }
 
-        const isCorrectPassword = bcrypt.compareSync(password, user.rows[0].password)
+        const isCorrectPassword = bcrypt.compareSync(password, user.password)
         if (!isCorrectPassword) {
             throw ApiError.BadRequest('Указан неверный пароль')
         }
-        
-        const rolesIds = await db.query('SELECT "roleID" FROM roles_for_users WHERE "userID" = $1;', [user.rows[0]._id])
-        let roles = []
-        for (let i = 0; i < rolesIds.rows.length; i++) {
-            const value = await db.query('SELECT value FROM roles WHERE _id = $1;', [rolesIds.rows[i].roleID])
-            roles.push(value.rows[0].value)
-        }
-        const dto = new UserDTO({...user.rows[0], roles: roles})
-        const tokens = tokenService.generateTokens({ ...dto })
-        await tokenService.saveToken(dto.id, tokens.refreshToken)
 
-        return {
-            ...tokens,
-            user: dto
-        }
+        return this.createResponse(user)
     }
 
     async logout(refreshToken) {
@@ -80,21 +57,8 @@ class UserService {
             throw ApiError.UnauthorizedError()
         }
 
-        const user = await db.query('SELECT * FROM users WHERE _id = $1', [userData.id])
-        const rolesIds = await db.query('SELECT "roleID" FROM roles_for_users WHERE "userID" = $1;', [userData.id])
-        let roles = []
-        for (let i = 0; i < rolesIds.rows.length; i++) {
-            const value = await db.query('SELECT value FROM roles WHERE _id = $1;', [rolesIds.rows[i].roleID])
-            roles.push(value.rows[0].value)
-        }
-        const dto = new UserDTO({...user.rows[0], roles: roles})
-        const tokens = tokenService.generateTokens({ ...dto })
-        await tokenService.saveToken(dto.id, tokens.refreshToken)
-
-        return {
-            ...tokens,
-            user: dto
-        }
+        const user = await this.getUserById(userData.id)
+        return this.createResponse(user)
     }
 
     async activate(activationLink) {
@@ -112,69 +76,37 @@ class UserService {
     }
 
     async changeUsername(id, username) {
-        let candidate = await db.query('SELECT * FROM users WHERE username = $1;', [username])
-        if (candidate.rows[0]) {
-            throw ApiError.BadRequest('Пользователь с таким логином уже существует')
-        }
-        let user = await db.query('SELECT FROM users WHERE _id = $1', [id])
-        if (!user.rows[0]) {
+        await this.checkUsernameAvailability(username)
+
+        let user = await this.getUserById(id)
+        if (!user) {
             throw new ApiError.BadRequest('Пользователь не найден')
         } else {
             user = await db.query('UPDATE users SET username = $1 WHERE _id = $2 RETURNING *;', [username, id])
             
-            const rolesIds = await db.query('SELECT "roleID" FROM roles_for_users WHERE "userID" = $1;', [id])
-            let roles = []
-            for (let i = 0; i < rolesIds.rows.length; i++) {
-                const value = await db.query('SELECT value FROM roles WHERE _id = $1;', [rolesIds.rows[i].roleID])
-                roles.push(value.rows[0].value)
-            }
-
-            const dto = new UserDTO({...user.rows[0], roles: roles})
-            const tokens = tokenService.generateTokens({ ...dto })
-            await tokenService.saveToken(dto.id, tokens.refreshToken)
-
-            return {
-                ...tokens,
-                user: dto
-            }
+            return this.createResponse(user.rows[0])
         }
     }
 
     async changeEmail(id, email) {
-        let candidate = await db.query('SELECT * FROM users WHERE email = $1;', [email])
-        if (candidate.rows[0]) {
-            throw ApiError.BadRequest('Пользователь с таким email уже существует')
-        }
-        let user = await db.query('SELECT FROM users WHERE _id = $1', [id])
+        await this.checkEmailAvailability(email)
+
+        let user = await this.getUserById(id)
         if (!user.rows[0]) {
             throw new ApiError.BadRequest('Пользователь не найден')
         } else {
             const activationLink = uuid.v4()
             
             user = await db.query('UPDATE users SET email = $1, "activationLink" = $2, "isActivated" = false WHERE _id = $3 RETURNING *;', [email, activationLink, id])
-            
-            const rolesIds = await db.query('SELECT "roleID" FROM roles_for_users WHERE "userID" = $1;', [id])
-            let roles = []
-            for (let i = 0; i < rolesIds.rows.length; i++) {
-                const value = await db.query('SELECT value FROM roles WHERE _id = $1;', [rolesIds.rows[i].roleID])
-                roles.push(value.rows[0].value)
-            }
 
             mailService.sendMail(email, `${process.env.HOST}/auth/activate/${activationLink}`)
 
-            const dto = new UserDTO({...user.rows[0], roles: roles})
-            const tokens = tokenService.generateTokens({ ...dto })
-            await tokenService.saveToken(dto.id, tokens.refreshToken)
-
-            return {
-                ...tokens,
-                user: dto
-            }
+            return this.createResponse(user.rows[0])
         }
     }
 
     async changePassword(id, password) {
-        let user = await db.query('SELECT FROM users WHERE _id = $1', [id])
+        let user = await this.getUserById(id)
         if (!user.rows[0]) {
             throw new ApiError.BadRequest('Пользователь не найден')
         } else {
@@ -182,21 +114,47 @@ class UserService {
             
             user = await db.query('UPDATE users SET password = $1 WHERE _id = $2 RETURNING *;', [hashPassword, id])
 
-            const rolesIds = await db.query('SELECT "roleID" FROM roles_for_users WHERE "userID" = $1;', [id])
-            let roles = []
-            for (let i = 0; i < rolesIds.rows.length; i++) {
-                const value = await db.query('SELECT value FROM roles WHERE _id = $1;', [rolesIds.rows[i].roleID])
-                roles.push(value.rows[0].value)
-            }
+            return this.createResponse(user.rows[0])
+        }
+    }
 
-            const dto = new UserDTO({...user.rows[0], roles: roles})
-            const tokens = tokenService.generateTokens({ ...dto })
-            await tokenService.saveToken(dto.id, tokens.refreshToken)
 
-            return {
-                ...tokens,
-                user: dto
-            }
+
+    async getUserById(id) {
+        const user = await db.query('SELECT * FROM users WHERE _id = $1;', [id])
+        return user.rows[0]
+    }
+
+    async getUserByUsername(username) {
+        const user = await db.query('SELECT * FROM users WHERE username = $1;', [username])
+        return user.rows[0]
+    }
+
+    async getUserRoles(userId) {
+        const rolesRows = await db.query('SELECT value FROM roles_for_users rfu JOIN roles r ON rfu."roleID" = r._id WHERE rfu."userID" = $1;', [userId])
+        return rolesRows.rows.map(row => row.value)
+    }
+
+    async createResponse(user) {
+        const roles = await this.getUserRoles(user._id)
+        const dto = new UserDTO({...user, roles})
+        const tokens = tokenService.generateTokens({...dto})
+        await tokenService.saveToken(dto.id, tokens.refreshToken)
+
+        return {...tokens, user: dto}
+    }
+
+    async checkUsernameAvailability(username) {
+        const candidate = await db.query('SELECT * FROM users WHERE username = $1;', [username])
+        if (candidate.rows.length > 0) {
+            throw ApiError.BadRequest('Пользователь с таким логином уже существует')
+        }
+    }
+
+    async checkEmailAvailability(email) {
+        const candidate = await db.query('SELECT * FROM users WHERE email = $1;', [email])
+        if (candidate.rows.length > 0) {
+            throw ApiError.BadRequest('Пользователь с таким email уже существует')
         }
     }
 }
